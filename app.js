b12c1a145d2a67596ef7badfac57b3c337f46b30
chromonav <chromonav@gmail.com>
2016-02-17 20:02:52 +0530

    adding dependencies

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var app = express();
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    publicDir = __dirname + '/public',
+    port = parseInt(process.env.PORT, 10) || 4567,
+    path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res) {
+    res.sendFile("public/index.html", { root: __dirname });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+}));
+
+
+app.listen(port, hostname);
