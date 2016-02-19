640d4540f66b589ab4d80d4a3d1230e567d92db8
chromonav <chromonav@gmail.com>
2016-02-19 20:31:40 +0530

    dbcommit

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var express = require("express"),
     publicDir = __dirname + '/public',
     port = parseInt(process.env.PORT, 10) || 4567,
     path = require('path');
-
+    
+//database setup
+    
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/pdcnode');
 
 app.use(methodOverride());
 app.use(bodyParser.json());         // support json encoded bodies
@@ -20,20 +24,42 @@ app.use(errorHandler({
     showStack: true
 }));
 
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var FeedbackSchema = new Schema({
+    id: ObjectId,
+    name: String,
+    email: String,
+    feedback: String
+});
+
+var FeedbackModel = mongoose.model('feedbacks', FeedbackSchema);
+
 app.get("/", function (req, res) {
     res.sendFile("public/index.html", { root: __dirname });
-  
 });
 
-app.post('/', function(req, res) {
-    console.dir(req.body);
+app.post('/', function (req, res) {
 
-      res.redirect("dashboard")
+    var feedbackmodelJson = req.body;
+
+    var feedbackmodel = new FeedbackModel(feedbackmodelJson);
+    feedbackmodel.save(function (err) {
+        if (err) return handleError(err);
+        // saved!
+    })
+    res.redirect("dashboard");
 });
-app.get('/dashboard', function(req, res) {
 
+app.get('/dashboard', function (req, res) {
+    FeedbackModel.find({}, function (err, docs) {
 
-      res.sendFile("public/dashboard.html",{ root: __dirname })
+        res.json(docs);
+    });
+
+ 
 });
 console.log("server running on:" + port);
 
